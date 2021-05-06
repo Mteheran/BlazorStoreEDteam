2f6a6ff965b11bb934552d0d26d757772c954b6f
Miguel Teheran <miguelteheran@gmail.com>
2021-05-05 22:51:28 -0500

    adding news module

---
+++ Shared/News.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace shared
+{
+    public class News
+    {
+        public Guid NewsId {get; set;} = Guid.NewGuid();
+        public string Description {get;set;}
+        public string Image {get;set;}
+        public string Url {get;set;}
+    }
+}
